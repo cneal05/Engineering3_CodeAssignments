2c3d5bbb06e5c35f19647a955fab694f200a1b81
Cole Neal <cneal05@ccs.local>
2021-09-30 09:45:08 -0400

    Added PhotoInterupterPython code again

--- PhotoInterupterPython.py
+++ PhotoInterupterPython.py
@@ -1,15 +1,27 @@
-import analogio
+from digitalio import DigitalInOut, Direction, Pull
 import time
 import board
 
-photocell = analogio.AnalogIn(board.A2)
+interrupter = DigitalInOut(board.D7)
+interrupter.direction = Direction.INPUT
+interrupter.pull = Pull.UP
 
 counter = 0
 
+photo = False
+state = False
+
+timer = 4
+start = time.time()
 while True:
-    photo = photocell.value
-    print(photo)
-    time.sleep(1)
-    if photo <= 250:
-        counter = counter + 1
-    print(counter)
+    photo = interrupter.value
+    if photo and not state:
+            counter += 1
+    state = photo
+
+    remaining = timer - time.time()
+
+    if remaining <= 0:
+        print("I have been interrupted", str(counter), "times")
+        timer = time.time() + 4
+        counter = 0
